32e63d1f085287bcadaeb898c724d13a511fcb03
João Ramajo <joaoramajo744@gmail.com>
2026-01-04 16:26:21 -0300

    fix(AppServiceProvider): remove force do https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Models\Expense;
 use App\Observers\ExpenseObserver;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,8 +21,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
     }
 }
