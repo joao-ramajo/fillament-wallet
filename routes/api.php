2f9f0e1af81e40b3e25c6b738684ceee63e5e280
João Ramajo <joaoramajo744@gmail.com>
2026-01-31 22:36:09 -0300

    feat: adiciona rota para baixar csv

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,13 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Dashboard\GenerateExpenseCsv;
 use App\Http\Controllers\Dashboard\GetExpensesController;
 use App\Http\Controllers\Dashboard\GetSummaryController;
 use App\Http\Controllers\Expense\CreateExpenseController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post("/expenses", CreateExpenseController::class)->middleware(['auth:sanctum'])->name('api.expenses.create');
+Route::post('/expenses', CreateExpenseController::class)->middleware(['auth:sanctum'])->name('api.expenses.create');
 Route::post('/register', RegisterController::class)->name('api.register');
 Route::post('/login', LoginController::class)->name('api.login');
 Route::get('/dashboard/summary', GetSummaryController::class)->middleware([
@@ -21,3 +17,4 @@ Route::get('/dashboard/summary', GetSummaryController::class)->middleware([
 Route::get('/dashboard/expenses', GetExpensesController::class)->middleware([
     'auth:sanctum'
 ])->name('api.get-expenses');
+Route::get('/dashboard/spreadsheet/csv/export', GenerateExpenseCsv::class)->middleware(['auth:sanctum'])->name('api.csv.export');
