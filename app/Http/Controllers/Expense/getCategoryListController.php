de375e41f255bc6555bb659986585b904c3582a0
João Ramajo <joaoramajo744@gmail.com>
2026-02-01 12:24:18 -0300

    fix: ajusta listagem de categorias para buscar o count correto

--- app/Http/Controllers/Expense/getCategoryListController.php
+++ app/Http/Controllers/Expense/getCategoryListController.php
@@ -11,6 +11,17 @@ class getCategoryListController extends Controller
 {
     public function __invoke()
     {
-        return Category::where('user_id', Auth::id())->withCount(['expenses'])->orWhereNull('user_id')->orderBy('name', 'asc')->get();
+        return Category::query()
+            ->where(function ($q) {
+                $q->where('user_id', Auth::id())
+                  ->orWhereNull('user_id');
+            })
+            ->withCount([
+                'expenses as expenses_count' => function ($q) {
+                    $q->where('user_id', Auth::id());
+                }
+            ])
+            ->orderBy('name', 'asc')
+            ->get();
     }
 }
