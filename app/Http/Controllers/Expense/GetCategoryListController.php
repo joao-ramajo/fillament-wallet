e72e4e21313a556726385050cec25b36659f8a3f
João Ramajo <joaoramajo744@gmail.com>
2026-02-08 11:51:52 -0300

    feat: adiciona total gasto por categoria

--- app/Http/Controllers/Expense/GetCategoryListController.php
+++ app/Http/Controllers/Expense/GetCategoryListController.php
@@ -13,13 +13,18 @@ class GetCategoryListController extends Controller
         return Category::query()
             ->where(function ($q) {
                 $q->where('user_id', Auth::id())
-                  ->orWhereNull('user_id');
+                ->orWhereNull('user_id');
             })
             ->withCount([
-                'expenses as expenses_count' => function ($q) {
-                    $q->where('user_id', Auth::id());
-                }
+            'expenses as expenses_count' => function ($q) {
+                $q->where('user_id', Auth::id());
+            }
             ])
+            ->withSum([
+            'expenses as expenses_total_amount' => function ($q) {
+                $q->where('user_id', Auth::id());
+            }
+            ], 'amount')
             ->orderBy('name', 'asc')
             ->get();
     }
